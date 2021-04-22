a0b9baf9855eb00e0fa7a5742918fae7532c1d50
Jiayu Li, Gary <54823746+Jiayuli-CU@users.noreply.github.com>
2021-04-22 20:17:43 +0800

    Add files via upload

---
+++ Backend/recommendation/RecommendationException.java
@@ -0,0 +1,7 @@
+package com.jiayuli.jupiter.recommendation;
+
+public class RecommendationException extends RuntimeException {
+    public RecommendationException(String errorMessage) {
+        super(errorMessage);
+    }
+}
